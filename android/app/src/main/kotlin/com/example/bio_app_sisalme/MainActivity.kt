40d7bdedd8314d914fdf82421716b0e184de88b7
mohamadabed <mhmmad.2237@gmail.com>
2022-08-26 15:14:30 +0300

    comit - First Comit

---
+++ android/app/src/main/kotlin/com/example/bio_app_sisalme/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bio_app_sisalme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class My_bio_app extends StatelessWidget {
+  const My_bio_app({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
